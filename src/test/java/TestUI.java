dced852d56d500e5cc4b6516af1208bf2f77fd49
Kamil <k.sabuev@datamart.ru>
2024-03-31 06:54:36 +0300

    add task 7

--- src/test/java/TestUI.java
+++ src/test/java/TestUI.java
@@ -1,106 +1,49 @@
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import widgets.*;
 
-import java.awt.*;
-import java.util.Map;
+import wigets.ContextMenu;
+import wigets.DragAndDrop;
+import wigets.InfiniteScroll;
+import wigets.KeyPresses;
+
 import java.util.stream.Stream;
-
-import static java.util.Arrays.asList;
 
 
 public class TestUI extends BaseTest {
 
-    static Stream<Arguments> checkboxData() {
-        return Stream.of(
-                Arguments.of(Map.of("checkbox 1", true,
-                        "checkbox 2", false)),
-                Arguments.of(Map.of("checkbox 2", false,
-                        "checkbox 1", true))
-        );
+    @Test()
+    @DisplayName("Drag and drop")
+    public void testDragAndDrop() {
+        new DragAndDrop()
+                .dragAndDrop("A", "B");
     }
 
-    @ParameterizedTest
-    @MethodSource("checkboxData")
-    public void testCheckbox(Map<String, Boolean> checkboxes) {
-        checkboxes.forEach((name, state) ->
-                new Checkboxes()
-                        .selectCheckbox(name)
-                        .checkStateCheckbox(name, state)
-                        .refresh()
-        );
+    @Test()
+    @DisplayName("Context menu")
+    public void testContextMenu() {
+        new ContextMenu()
+                .clickHotSpot()
+                .checkContextMenuContainsText("You selected a context menu");
     }
 
-
-    @ParameterizedTest(name = "Line number {0} has the value: {1}")
-    @CsvSource({
-            "'1', 'Option 1'",
-            "'2', 'Option 2'",
-    })
-    public void testDropdown(String num, String value) {
-        new Dropdown()
-                .selectDropdownByNum(num)
-                .checkState(value);
+    @Test()
+    @DisplayName("Infinite scroll")
+    public void testInfiniteScroll() {
+        new InfiniteScroll()
+                .scrollTextAndSearch("Eius");
     }
 
-    @Test
-    @RepeatedTest(value = 10, name = "Disappearing elements: {currentRepetition} out of {totalRepetitions}")
-    public void testDisappearingElements() {
-        new DisappearingElements()
-                .checkNumberOfElements(5);
+    static Stream<String> keyPressesData() {
+        return Stream.of("ENTER", "CONTROL", "ALT", "TAB","A","B","C","D","E","f","g","x","w","z");
     }
 
-    @TestFactory
-    public Stream<DynamicTest> testInputs() {
-        Inputs inputs = new Inputs();
-        return Stream.concat(
-                inputs.checkValidInputs(asList("-99", "-1", "0", "1", "9e+18")),
-                inputs.checkInvalidInputs(asList("abc", "!@#", " 123", "456 ", "1q"))
-        );
-    }
-
-    @ParameterizedTest(name = "Hovers with index {0} and text: {1}")
-    @CsvSource({
-            "0, 'name: user1'",
-            "1, 'name: user2'",
-            "2, 'name: user3'",
-    })
-    public void testHovers(Integer index, String text) {
-        new Hovers()
-                .highlightFigure(index).
-                checkTextSelectedFigure(text);
-    }
-
-    @RepeatedTest(value = 10, name = "Notification message: {currentRepetition} of {totalRepetitions} ")
-    public void testNotificationMessages() {
-        new NotificationMessage()
-                .clickHere()
-                .checkForMessage("Action successful");
-    }
-
-    @TestFactory
-    public Stream<DynamicTest> testAddRemoveElements() {
-        return new AddRemoveElements()
-                .addAndDeleteElements(asList("2:1", "5:2", "1:3"));
-    }
-
-    @ParameterizedTest(name = "StatusCodes: {0}")
-    @CsvSource({
-            "200",
-            "301",
-            "404",
-            "500",
-    })
-    public void testStatusCodes(String code) {
-        new StatusCodes()
-                .clickOnStatusCode(code)
-                .checkContainsUrl(code);
+    @ParameterizedTest(name = "Key {0} presses")
+    @MethodSource("keyPressesData")
+    public void testKeyPresses(String keys) {
+        new KeyPresses()
+                .keyPresses(keys)
+                .checkKey(keys);
     }
 
 
